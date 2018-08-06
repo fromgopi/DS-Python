6ff8ad4ee05fe940de779bc8d3786a66948c4f5c
Gopi Krishna Muktevi <muktevigk@handbookinc.com>
2018-08-07 00:35:46 +0530

    WIP

---
+++ src/linkedlists/LinkedList.py
@@ -0,0 +1,15 @@
+from src.linkedlists import ListNode as node
+
+
+class LinkedList(object):
+
+    def rotateList(self, head, k):
+        if head is None or head.next is None:
+            return head
+
+
+
+if __name__ == '__main__':
+
+    newNode = node.ListNode()
+    print(newNode.val)

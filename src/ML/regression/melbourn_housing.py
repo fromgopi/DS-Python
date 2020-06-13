1f2ba05c8f43885834d05f1417b5c88d0a6a3e89
Gopi Krishna M <gopt.muktevi@gmail.com>
2020-06-13 22:34:23 +0530

    Added some regression Problems with data

---
+++ src/ML/regression/melbourn_housing.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+melbourne_housing_data_path = '../data/melb_data.csv'
+melbourne_housing_data = pd.read_csv(melbourne_housing_data_path)
+
+print(melbourne_housing_data.describe())
+print(melbourne_housing_data.columns)

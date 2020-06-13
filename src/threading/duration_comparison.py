1fffed4a42a71caaeb4de1918251be0cca06f9bc
Gopi Krishna M <fromgopi@yahoo.com>
2020-06-13 22:30:51 +0530

    Adding some multiThreading and Multiprocessing Examples
    Adding some multiThreading and Multiprocessing Examples

---
+++ src/threading/duration_comparison.py
@@ -0,0 +1,48 @@
+import time
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def multithreading(func, args, workers):
+    with ThreadPoolExecutor(workers) as worker:
+        res = worker.map(func, args)
+    return list(res)
+
+
+def multiprocessing(func, args, workers):
+    with ProcessPoolExecutor(workers) as ex:
+        res = ex.map(func, args)
+    return list(res)
+
+
+def cpu_heavy(x):
+    print('I am', x)
+    start = time.time()
+    count = 0
+    for i in range(10 ** 8):
+        count += i
+    stop = time.time()
+    return start, stop
+
+
+def visualize_runtimes(results, title):
+    start, stop = np.array(results).T
+    plt.barh(range(len(start)), stop - start)
+    plt.grid(axis='x')
+    plt.ylabel("Tasks")
+    plt.xlabel("Seconds")
+    plt.xlim(0, 22.5)
+    ytks = range(len(results))
+    plt.yticks(ytks, ['job {}'.format(exp) for exp in ytks])
+    plt.title(title)
+    return stop[-1] - start[0]
+
+
+if __name__ == '__main__':
+    plt.subplot(1, 2, 1)
+    visualize_runtimes(multithreading(cpu_heavy, range(5), 5), "Multithreading")
+    plt.subplot(1, 2, 2)
+    visualize_runtimes(multiprocessing(cpu_heavy, range(5), 5), "Multiprocessing")
+    plt.show()

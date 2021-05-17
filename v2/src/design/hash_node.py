6a5be16b9bd4a064ad38ba26f9d6a667a42b80eb
Gopi Krishna M <gopt.muktevi@gmail.com>
2021-05-17 21:17:42 +0530

    Pushing some dynamic programming probs

---
+++ v2/src/design/hash_node.py
@@ -0,0 +1,17 @@
+class HashNode:
+    """
+    Hash Node class
+    """
+    def __init__(self, key, value):
+        """
+        Constructor for HashNode
+        """
+        self.key = key
+        self.value = value
+        self.next = None
+    
+    def __str__(self):
+        return "<Node: (%s, %s), next: %s>" % (self.key, self.value, self.next != None)
+    
+    def __repr__(self):
+        return str(self)
